46ceb3198a933b1dec4329dbac52771459dbdd80
Michael Egorov <michwill@gmail.com>
2015-12-31 09:38:34 -0800

    Benchmarks script with pandas dataframes

--- zerodb-benchmarks/run.py
+++ zerodb-benchmarks/run.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python2
 
+import click
 import time
 import sys
-import benchmarks
 import pandas
 import zerodb
+
+import benchmarks
+
+from os import path
+from datetime import datetime
 
 SOCKET = ("localhost", 8001)
 username = "test"
 password = "testpassword"
+DATE_FMT = "%Y-%m-%d-%H-%M"
 
 
-def run_benchmark(cls, methodname, n_subcycles=50, n_cycles=1000, **kw):
+def run_benchmark(cls, methodname, n_subcycles=5, n_cycles=1000, output_dir="out", **kw):
+    fname = path.join("out", "{0}.{1}.{2}.csv".format(cls.__name__, methodname, datetime.utcnow().strftime(DATE_FMT)))
     db = zerodb.DB(SOCKET, username=username, password=password)
     bench = cls(db, **kw)
     method = getattr(bench, methodname)
@@ -28,6 +35,8 @@ def run_benchmark(cls, methodname, n_subcycles=50, n_cycles=1000, **kw):
             "n_queries": (i + 1) * n_subcycles}
         print i, d_out
         out.append(d_out)
+        df = pandas.DataFrame(out)
+        df.to_csv(fname)
     db.disconnect()
     return out
 
@@ -35,8 +44,5 @@ def run_benchmark(cls, methodname, n_subcycles=50, n_cycles=1000, **kw):
 if __name__ == "__main__":
     classname = sys.argv[1] if len(sys.argv) > 1 else "TextBenchmark"
     methodname = sys.argv[2] if len(sys.argv) > 2 else "read_one"
-    fname = "{0}.{1}.csv".format(classname, methodname)
     cls = getattr(benchmarks, classname)
-
-    df = pandas.DataFrame(run_benchmark(cls, methodname))
-    df.to_csv(fname)
+    run_benchmark(cls, methodname)
