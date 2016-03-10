fc6e41aa264d1d015989e8bdc0adc54720ceff21
Michael Egorov <michwill@gmail.com>
2016-03-10 15:52:42 +0000

    Index files in a new process to prevent memory leak (temporary measure)

--- zerodb-benchmarks/create_wiki.py
+++ zerodb-benchmarks/create_wiki.py
@@ -2,6 +2,7 @@
 
 import click
 import transaction
+import multiprocessing as mp
 
 from util.server import server
 from benchmarks.models import WikiPage
@@ -9,26 +10,35 @@ from wiki import read_docs
 
 
 @click.command()
-@click.option("--batch-size", default=50, type=click.INT, help="Number of records to add in one transaction", show_default=True)
+@click.option("--batch-size", default=10000, type=click.INT, help="Number of records to add in one transaction", show_default=True)
 @click.option("--db-dir", type=click.STRING, default="", help="Directory for the db")
 @click.option("--wiki-dir", type=click.STRING, default="wiki_dump", help="Directory with Wikipedia plaintext dump")
 def run(batch_size, db_dir, wiki_dir):
     click.echo("Counting number of docs...")
-    bctr = 0
 
     with server(db_dir=db_dir, compress=True) as db:
-        for doc in read_docs(wiki_dir):
-            page = WikiPage(**doc)
-            db.add(page)
+        def indexer(docs):
+            with transaction.manager:
+                for doc in docs:
+                    db.add(WikiPage(**doc))
+            db.pack()
 
-            bctr += 1
-            if bctr >= batch_size:
-                transaction.commit()
-                bctr = 0
-                db.pack()
+        it = read_docs(wiki_dir)
 
-        transaction.commit()
-        db.pack()
+        while True:
+            docs = []
+            last = False
+            try:
+                for i in range(batch_size):
+                    docs.append(next(it))
+            except StopIteration:
+                last = True
+            if docs:
+                p = mp.Process(target=indexer, args=(docs,))
+                p.start()
+                p.join()
+            if last:
+                break
 
     click.echo("Stopped the db")
 
