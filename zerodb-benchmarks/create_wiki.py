3bc6b2dea17b05a822198850b8424764c381a731
Michael Egorov <michwill@gmail.com>
2016-03-11 11:14:56 +0000

    Adapt to the memory available

--- zerodb-benchmarks/create_wiki.py
+++ zerodb-benchmarks/create_wiki.py
@@ -1,44 +1,70 @@
 #!/usr/bin/env python2
 
 import click
+import path
 import transaction
+import psutil
+import os
+import gc
 import multiprocessing as mp
+
+from zerodb.models import Model, Field
 
 from util.server import server
 from benchmarks.models import WikiPage
-from wiki import read_docs
+from wiki import read_doc
+
+
+COMMIT_THRESHOLD = 55.0
+TERM_THRESHOLD = 30.0
+
+
+class PageName(Model):
+    processed = Field(default=False)
+    fname = Field()
 
 
 @click.command()
-@click.option("--batch-size", default=10000, type=click.INT, help="Number of records to add in one transaction", show_default=True)
 @click.option("--db-dir", type=click.STRING, default="", help="Directory for the db")
 @click.option("--wiki-dir", type=click.STRING, default="wiki_dump", help="Directory with Wikipedia plaintext dump")
-def run(batch_size, db_dir, wiki_dir):
-    click.echo("Counting number of docs...")
-
+def run(db_dir, wiki_dir):
     with server(db_dir=db_dir, compress=True) as db:
-        def indexer(docs):
-            with transaction.manager:
-                for doc in docs:
+
+        def indexer():
+            process = psutil.Process(os.getpid())
+            names_to_process = list(db[PageName].query(processed=False))
+            total_names = len(db[PageName])
+
+            for i, p in enumerate(names_to_process):
+                print(1.0 - (float(len(names_to_process) - i) / total_names))
+                for doc in read_doc(p.fname):
                     db.add(WikiPage(**doc))
+                pname = p
+                pname.processed = True
+                if process.memory_percent() > COMMIT_THRESHOLD:
+                    transaction.commit()
+                    gc.collect()
+                    if process.memory_percent() > TERM_THRESHOLD:
+                        db.pack()
+                        return
+
+            print "All done"
+            transaction.commit()
             db.pack()
 
-        it = read_docs(wiki_dir)
+        ctr = 0
+        for fname in map(str, path.path(wiki_dir).walkfiles("*.bz2")):
+            if not db[PageName].query(fname=fname):
+                db[PageName].add(PageName(fname=fname))
+                ctr += 1
+        transaction.commit()
+        print("Added {0} files, total: {1}".format(ctr, len(db[PageName])))
 
-        while True:
-            docs = []
-            last = False
-            try:
-                for i in range(batch_size):
-                    docs.append(next(it))
-            except StopIteration:
-                last = True
-            if docs:
-                p = mp.Process(target=indexer, args=(docs,))
-                p.start()
-                p.join()
-            if last:
-                break
+        while db[PageName].query(processed=False, limit=1):
+            p = mp.Process(target=indexer)
+            p.start()
+            p.join()
+            transaction.abort()  # Need beecause of MVCC
 
     click.echo("Stopped the db")
 
--- zerodb-benchmarks/wiki.py
+++ zerodb-benchmarks/wiki.py
@@ -7,27 +7,32 @@ head_re = re.compile(r'\<doc id="(\d+)" url="(.*)" title="(.*)"\>')
 tail = "</doc>\n"
 
 
+def read_doc(fname):
+    f = bz2.BZ2File(str(fname))
+    text = None
+    for line in f:
+        if text is None:
+            r = head_re.match(line)
+            if r:
+                docid, url, title = r.groups()
+                text = []
+        else:
+            if line == tail:
+                yield {"id": docid,
+                       "url": url,
+                       "title": title,
+                       "text": "".join(text)}
+                text = None
+            else:
+                text.append(line)
+    f.close()
+
+
 def read_docs(dirname="wiki_sample"):
     with click.progressbar(list(path.path(dirname).walkfiles("*.bz2")), label="Creating test records") as bar:
         for fname in bar:
-            f = bz2.BZ2File(fname)
-            text = None
-            for line in f:
-                if text is None:
-                    r = head_re.match(line)
-                    if r:
-                        docid, url, title = r.groups()
-                        text = []
-                else:
-                    if line == tail:
-                        yield {"id": docid,
-                               "url": url,
-                               "title": title,
-                               "text": "".join(text)}
-                        text = None
-                    else:
-                        text.append(line)
-            f.close()
+            for doc in read_doc(fname):
+                yield doc
 
 
 if __name__ == "__main__":
